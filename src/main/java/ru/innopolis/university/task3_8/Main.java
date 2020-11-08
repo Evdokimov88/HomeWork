77b4623db2feddfbe5b188be1ab672bcdd855360
Evdokimo88 <mail.evdokimov@yandex.ru>
2020-11-08 21:15:51 +0300

    ДЗ 3_8

--- src/main/java/ru/innopolis/university/task3_8/Main.java
+++ src/main/java/ru/innopolis/university/task3_8/Main.java
@@ -6,12 +6,14 @@ public class Main{
         int[] array = new int[(int) (Math.random() * 100)];
         for (int j = 0; j < array.length; j++) {
             array[j] = (int) (Math.random() * 100);
+            System.out.println("Вычисляем факториал числа "+array[j]);
             NewThread thread = new NewThread(array[j]);
             thread.t1.start();
             thread.t2.start();
             thread.t1.join();
             thread.t2.join();
             thread.t3.start();
+            thread.t3.join();
         }
     }
 }
--- src/main/java/ru/innopolis/university/task3_8/NewThread.java
+++ src/main/java/ru/innopolis/university/task3_8/NewThread.java
@@ -8,32 +8,23 @@ public class NewThread{
     BigInteger fact1;
     BigInteger fact2;
 
-
     public NewThread(int arr) {
-
         this.arr=arr;
-
-
     }
 
-
-
     Thread t1 = new Thread("t1") {
-        @Override
         public void run() {
             System.out.println("Running t1");
-
             BigInteger fact = BigInteger.valueOf(1);
             for (int i = 2; i <= arr; i += 2) {
                 fact = fact.multiply(BigInteger.valueOf(i));
             }
             System.out.println(fact);
             fact1 = fact;
-
         }
     };
+
     Thread t2 = new Thread("t2") {
-        @Override
         public void run() {
             System.out.println("Running t2 ");
             BigInteger fact = BigInteger.valueOf(1);
@@ -43,8 +34,8 @@ public class NewThread{
             System.out.println(fact);
             fact2 = fact;
         }
-
     };
+
     Thread t3 = new Thread("t3") {
         public void run() {
             BigInteger res;
@@ -52,7 +43,6 @@ public class NewThread{
             res = fact1.multiply(fact2);
             System.out.println(res);
         }
-
     };
 
 }
