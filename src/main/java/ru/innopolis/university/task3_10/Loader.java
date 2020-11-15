8a1501fcd396c3a8835e62b84ae539a75be386b6
Andrey <mail.evdokimov@mail.ru>
2020-11-15 20:37:48 +0300

    ДЗ3_10

--- src/main/java/ru/innopolis/university/task3_10/Loader.java
+++ src/main/java/ru/innopolis/university/task3_10/Loader.java
@@ -1,53 +1,55 @@
 package ru.innopolis.university.task3_10;
 
+
+import javax.tools.JavaCompiler;
+import javax.tools.ToolProvider;
 import java.io.*;
 import java.util.Scanner;
 
-public class Loader extends ClassLoader{
+public class Loader extends ClassLoader {
     public static void main(String[] args) throws IOException, ClassNotFoundException {
+        String head = "package ru.innopolis.university.task3_10;\n" +
+                "public class SomeClass implements Worker{\n" +
+                "    @Override\n" +
+                "    public void doWork() {";
 
         System.out.println("Введите код для выполнения");
-        //BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         Scanner scanner = new Scanner(System.in);
 
         File file = new File("SomeClass.java");
         file.createNewFile();
-        try (FileOutputStream fos = new FileOutputStream("SomeClass.java")) {
+        try (FileOutputStream fos =
+                     new FileOutputStream("SomeClass.java")) {
             while (scanner.hasNext()) {
-                fos.write((scanner.nextLine()).getBytes());
+
+                fos.write((head + scanner.nextLine() + "} }").getBytes());
                 if ((scanner.nextLine()).equals(""))
+
                     break;
             }
         }
-        final Class simpleClass = loadSimpleClass();
+      //  JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
+       // compiler.run(null, null, null, "SomeClass.java");
+       // final Class someClass = loadSimpleClass();
+        Process p1 = Runtime.getRuntime().exec("javac",null, new File("C:\\Users\\Olga\\IdeaProjects\\HomeWork\\SomeClass"));
     }
 
 
     private static Class loadSimpleClass() throws ClassNotFoundException {
         MyClassLoader myClassLoader = new MyClassLoader();
+        final Class<?> someClass = Class.forName("SomeClass", true,myClassLoader);
 
-        final Class<?> simpleClass = Class.forName("SomeClass", true, myClassLoader);
-
-        System.out.println(simpleClass);
+        System.out.println(someClass);
 
         System.out.println("Класслоадер работает " +
-                (Class.forName("Simple", true, myClassLoader).equals(simpleClass) ?
+                (Class.forName("SomeClass", true, myClassLoader).equals(someClass) ?
                         "ВЕРНО" :
                         "НЕВЕРНО"));
 
-        return simpleClass;
+        return someClass;
     }
 }
 
-
-//package ru.innopolis.university.task3_10;
-//
-//public class SomeClass implements Worker{
-//    @Override
-//    public void doWork() {
-//
-//    }
-//}
 
 
 
